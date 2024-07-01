75d8e86c146b18fd94038df9d667d482147cd18f
Luckyshakthi <155925849+Luckyshakthi@users.noreply.github.com>
2024-07-01 08:43:10 -0700

    starter index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Make sure you create this CSS file or remove this import
+import App from './app'; // Importing the App component
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Make sure you have a div with id="root" in your public/index.html
+);
