c843a432c29720f7a80c24c6a6a19423d198655e
Luckyshakthi <155925849+Luckyshakthi@users.noreply.github.com>
2024-07-01 08:39:09 -0700

    starter code

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import './App.css'; // Make sure you create this CSS file or remove this import
+import Component from './component'; // Importing the Component
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+        <Component /> {/* Using the Component */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
