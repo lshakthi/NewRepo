42a4b39c550408befa080e22150f412b1bdecaba
Luckyshakthi <155925849+Luckyshakthi@users.noreply.github.com>
2024-07-01 08:43:56 -0700

    starter component

--- frontend/src/component.jsx
+++ frontend/src/component.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Component = () => {
+  return (
+    <div>
+      <h2>This is a component</h2>
+      <p>Components help you split the UI into independent, reusable pieces.</p>
+    </div>
+  );
+};
+
+export default Component;
